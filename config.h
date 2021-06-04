662f8d9dd72ba5e59d7c11719bf9f4ef12f4d3ac
m3dry <m3dry@protonmail.com>
2021-06-04 17:28:15 +0200

    Add ligature patch

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mononoki Nerd Font:pixelsize=17:antialias=true:autohint=true";
+static char *font = "Operator Mono SSm Lig:pixelsize=17:antialias=true:autohint=true";
 static char *font2[] = { "JoyPixels:pixelsize=15:antialias=true:autohint=true" };
 
 /* disable bold, italic and roman fonts globally */
